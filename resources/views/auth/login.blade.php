81f9ffffba7fa95e16c9ee9c90f7b12342c7e48b
khalil fakhfekh <khalil.fakhfekh@esprit.tn>
2019-03-21 23:54:04 +0100

    login page with assets

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,99 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Gestion Scolarite - Connexion</title>
+    <!-- Favicon and touch icons -->
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+    <!-- Start Global Mandatory Style
+         =====================================================================-->
+    <!-- Material Icons CSS -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="{{asset('auth/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet" type="text/css" />
+    <!-- Animation Css -->
+    <link href="{{asset('auth/animate/animate.css')}}" rel="stylesheet" />
+    <!-- materialize css -->
+    <link href="{{asset('auth/materialize/css/materialize.min.css')}}" rel="stylesheet">
+    <!-- custom CSS -->
+    <link href="{{asset('auth/css/stylematerial.css')}}" rel="stylesheet">
+</head>
 
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+<body class="sign-section">
+<div class="container sign-cont animated zoomIn">
+    <div class="row sign-row">
+        <div class="sign-title">
+            <h2 class="teal-text"><i class="fa fa-user-circle-o"></i></h2>
+            <h2 class="teal-text">Gestion Scolarite Login</h2>
+        </div>
+        <form class="col s12" id="reg-form" method="POST" action="{{ route('login') }}">
+            @csrf
+            <div class="row sign-row">
+                <div class="input-field col s12">
+                    <input id="u_name" type="email"  name="email" value="{{ old('email') }}" class="validate {{ $errors->has('email') ? ' is-invalid' : '' }}" required autofocus>
+                    <label for="u_name">{{ __('E-Mail Address') }}</label>
+                    @if ($errors->has('email'))
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                    @endif
                 </div>
             </div>
-        </div>
+            <div class="row sign-row">
+                <div class="input-field col s12">
+                    <input name="password" id="password" type="password" class="validate {{ $errors->has('password') ? ' is-invalid' : '' }}" required>
+                    <label for="password">{{ __('Password') }}</label>
+                    @if ($errors->has('password'))
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                    @endif
+                </div>
+            </div>
+            @if (Route::has('password.request'))
+                <div class="row sign-row">
+                    <div class="input-field col s12 m-b-30">
+                        <label class="pull-left">
+                            <a class='pink-text' href="{{ route('password.request') }}">
+                                <b>{{ __('Forgot Your Password?') }}</b>
+                            </a>
+                        </label>
+                    </div>
+                </div>
+            @endif
+            <div class="row sign-row">
+                <div class="input-field col s6">
+                    <div class="sign-confirm">
+                        <input type="checkbox" id="sign-confirm" name="remember" {{ old('remember') ? 'checked' : '' }}/>
+                        <label for="sign-confirm">{{ __('Remember Me') }}</label>
+                    </div>
+                </div>
+                <div class="input-field col s6">
+                    <button class="btn btn-large btn-register waves-effect waves-light green" type="submit" name="action">Login
+                        <i class="material-icons right">done_all</i>
+                    </button>
+                </div>
+            </div>
+        </form>
     </div>
 </div>
-@endsection
+
+<!-- Start Core Plugins
+     =====================================================================-->
+<!-- jQuery -->
+<script src="{{asset('auth/jQuery/jquery-3.2.1.min.js')}}" type="text/javascript"></script>
+<!-- materialize  -->
+<script src="{{asset('auth/materialize/js/materialize.min.js')}}" type="text/javascript"></script>
+<!-- End Core Plugins
+     =====================================================================-->
+<script>
+    $(document).ready(function() {
+        $('select').material_select();
+    });
+</script>
+</body>
+
+</html>
